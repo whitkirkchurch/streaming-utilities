5aae3f5a6c5266044760aa4ee8334e17a0b1c11c
Nick Jackson <nick@dxw.com>
2023-01-09 17:00:56 +0000

    Support service titles spanning multiple lines

--- generators/youtube_thumbnails.py
+++ generators/youtube_thumbnails.py
@@ -9,7 +9,8 @@ FONT_LATO_REGULAR_MD = ImageFont.truetype("fonts/Lato-Regular.ttf", 42)
 GENERATOR_VERSION = 3
 
 TARGET_THUMBNAIL_DIMENSIONS = (1280, 720)
-HORIZONTAL_MARGIN = 30
+LEFT_MARGIN = 30
+RIGHT_MARGIN = 120
 BOTTOM_MARGIN = 90
 SPACING_BETWEEN_TEXT = 8
 
@@ -61,31 +62,65 @@ class YoutubeThumbnail:
             main_text = self.service.title_string
             aux_text = self.service.datetime.strftime("%-d %B %Y")
 
-            # Figure out the bounding boxes for our text
-            main_text_bounding = FONT_LATO_BOLD_LG.getbbox(main_text, anchor="ld")
-            main_text_height = -main_text_bounding[1]
-
-            print(main_text_bounding)
-
             main_text_draw_coordinates = (
-                HORIZONTAL_MARGIN,
+                LEFT_MARGIN,
                 TARGET_THUMBNAIL_DIMENSIONS[1] - 10 - BOTTOM_MARGIN,
             )
 
-            aux_text_draw_coordinates = (
-                HORIZONTAL_MARGIN,
-                TARGET_THUMBNAIL_DIMENSIONS[1]
-                - main_text_height
-                - BOTTOM_MARGIN
-                - SPACING_BETWEEN_TEXT,
+            # Figure out the bounding boxes for our main text
+            main_text_bounding = draw.textbbox(
+                main_text_draw_coordinates,
+                main_text,
+                anchor="ld",
+                font=FONT_LATO_BOLD_LG,
             )
 
-            draw.text(
+            main_text_max_width = (
+                TARGET_THUMBNAIL_DIMENSIONS[0] - LEFT_MARGIN - RIGHT_MARGIN
+            )
+
+            # Is the text wider than our margins? If not, rock on. If it is, we need to do some wrapping.
+            if main_text_bounding[2] > main_text_max_width:
+                # Start with an empty string
+                main_text_with_breaks = ""
+
+                for word in main_text.split():
+                    # Figure out the size of the box with the new word
+                    text_to_test = main_text_with_breaks + word
+                    main_text_bounding = draw.multiline_textbbox(
+                        main_text_draw_coordinates,
+                        text_to_test,
+                        anchor="ld",
+                        font=FONT_LATO_BOLD_LG,
+                    )
+
+                    if main_text_bounding[2] > main_text_max_width:
+                        # It's too big, throw in a break
+                        main_text_with_breaks += "\n"
+
+                    main_text_with_breaks += word + " "
+
+                # We're done calculating breakpoints, move the broken text to the main variable
+                # and recalculate bounding box
+                main_text = main_text_with_breaks
+                main_text_bounding = draw.multiline_textbbox(
+                    main_text_draw_coordinates,
+                    main_text,
+                    anchor="ld",
+                    font=FONT_LATO_BOLD_LG,
+                )
+
+            aux_text_draw_coordinates = (
+                LEFT_MARGIN,
+                main_text_bounding[1] - SPACING_BETWEEN_TEXT,
+            )
+
+            draw.multiline_text(
                 xy=main_text_draw_coordinates,
                 text=main_text,
                 fill="#030303",
                 font=FONT_LATO_BOLD_LG,
-                anchor="ls",
+                anchor="ld",
             )
             draw.text(
                 xy=aux_text_draw_coordinates,
@@ -106,7 +141,7 @@ class YoutubeThumbnail:
                 text=main_text,
                 fill="#FFF",
                 font=FONT_LATO_BOLD_LG,
-                anchor="ls",
+                anchor="ld",
             )
             draw.text(
                 xy=aux_text_draw_coordinates,
